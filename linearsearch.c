fc17c1d79de4c3754492ff26d840dc7ddd9056a3
Ashly Rose Antony <75793225+ASHLY0115@users.noreply.github.com>
2021-12-17 10:29:49 +0530

    Add files via upload

--- linearsearch.c
+++ linearsearch.c
@@ -1,35 +1,38 @@
-/*****************************************
-File   :  linear Search
-Author : Ashly Rose Antony
-Date   : 29/11/2021
-Versio :1.0
-********************************************/
-#include <stdio.h>
-int main()
-{
-  int array[100], search, c, n;
-
-  printf("Enter number of elements in array\n");
-  scanf("%d", &n);
-
-  printf("Enter %d integer(s)\n", n);
-
-  for (c = 0; c < n; c++)
-    scanf("%d", &array[c]);
-
-  printf("Enter a number to search\n");
-  scanf("%d", &search);
-
-  for (c = 0; c < n; c++)
-  {
-    if (array[c] == search)    /* If required element is found */
-    {
-      printf("%d is present at location %d.\n", search, c+1);
-      break;
-    }
-  }
-  if (c == n)
-    printf("%d isn't present in the array.\n", search);
-
-  return 0;
-}
+/*********************************
+ file    : Linear search
+ author  : Ashly Rose Antony
+ version : 1.0
+ date    : 29/11/21
+ **********************************/
+#include<stdio.h>
+int main(){
+    int n,i,element,count=0;
+    int flag=0;
+    count++;
+    printf("\n Enter the no. of elements in array:");
+    scanf("%d",&n);
+    int array[10];
+    printf("\n Enter the elements of array :");
+    
+    for (i=0;i<n;i++){
+        printf("\n Enter element %d :",i+1);
+        scanf("%d",&array[i]);
+        }
+        printf("Enter the element to be search:");
+        scanf("%d",&element);
+    for(i=0;i<n;i++){
+    if(array[i] == element){
+       
+        count++;
+        printf("\n found element at position %d",i);
+        flag = 1;
+     }
+     count++;
+     }
+     if(flag == 0){   
+     printf("\n element not found ");
+     }
+     printf("\n time complexity = %d",count);
+     printf("\n space complexity=56");
+     return 0;
+     }     
