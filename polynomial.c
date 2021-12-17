67d0f9f1a54bfe4108102da67b78a0d7b0a1cd1a
Ashly Rose Antony <75793225+ASHLY0115@users.noreply.github.com>
2021-12-17 06:21:07 +0530

    Add files via upload

---
+++ polynomial.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#define base 100
+
+struct poly{
+	float coeff;
+	int exp;
+	};
+
+int main()
+{
+	struct poly a[base];
+	//b[base];
+	int p;
+	int i;
+	int k=0;
+	printf("Enter the highest degree of FIRST polynomial:  ");
+	scanf("%d",&p);
+	for(i=0;i<=p;i++)
+	{
+		printf("Enter the coefficient of x^%d : ",i);
+		scanf("%f",&a[i].coeff);
+		a[k++].exp=i;
+	}	
+
+	printf("The Polynomial is:");
+	for(i=p;i>=0;i--)
+	{
+	printf("+%.1fX^%d",a[i].coeff,a[i].exp);
+	}	
+	return 0;
+	}
+ 
