3fc055b256af9f386e88fd6dba158afe6e7352d3
Ashly Rose Antony <75793225+ASHLY0115@users.noreply.github.com>
2021-12-17 10:32:08 +0530

    Add files via upload

--- sparsematrix.c
+++ sparsematrix.c
@@ -1,66 +1,43 @@
-#include <stdio.h>
-#define MAX 20
-
-void read_matrix(int a[10][10], int row, int column);
-void print_sparse(int b[MAX][3]);
-void create_sparse(int a[10][10], int row, int column, int b[MAX][3]);
-
-int main()
-{
-    int a[10][10], b[MAX][3], row, column;
-    printf("\nEnter the size of matrix (rows, columns): ");
-    scanf("%d%d", &row, &column);
-
-    read_matrix(a, row, column);
-    create_sparse(a, row, column, b);
-    print_sparse(b);
-    return 0;
-}
-
-void read_matrix(int a[10][10], int row, int column)
-{
-    int i, j;
-    printf("\nEnter elements of matrix\n");
-    for (i = 0; i < row; i++)
-    {
-        for (j = 0; j < column; j++)
-        {
-            printf("[%d][%d]: ", i, j);
-            scanf("%d", &a[i][j]);
-        }
+/*********************************
+ file    : Sparse matrix
+ author  : Ashly Rose Antony
+ version : 1.0
+ date    : 3/12/21
+ **********************************/
+#include<stdio.h>
+int main(){
+ int myMatrix[10][10],i,j,rows,col,sparseMatrix[30][3],count=0;
+ printf("Enter the no. of rows and columns\n");
+ scanf("%d %d",&rows,&col);
+ printf("Enter the elements\n");
+ for(i=0;i<rows;i++){
+  for(j=0;j<col;j++){
+   printf("Element[%d][%d] :- ",i,j);
+   scanf("%d",&myMatrix[i][j]);
+  }
+ }
+ printf("\nYour matrix :- \n\n"); 
+ for(i=0;i<rows;i++){
+  for(j=0;j<col;j++){
+   printf("%d ",myMatrix[i][j]);
+  }
+  printf("\n");
+ }
+  for(i=0;i<rows;i++){
+   for(j=0;j<col;j++){
+    if(myMatrix[i][j]!=0){
+     count++;
+     sparseMatrix[count][0]=i;
+     sparseMatrix[count][1]=j;
+     sparseMatrix[count][2]=myMatrix[i][j];
     }
+   }
+  }
+  sparseMatrix[0][2]=count;
+  printf("Row\tCloumn\tValues\n\n");
+  printf("%d\t%d\t%d\n\n",rows,col,count);
+  for(i=1;i<=count;i++){
+   printf("%d\t%d\t%d\n",sparseMatrix[i][0],sparseMatrix[i][1],sparseMatrix[i][2]);
+  }
+ return 0;
 }
-
-void create_sparse(int a[10][10], int row, int column, int b[MAX][3])
-{
-    int i, j, k;
-    k = 1;
-    b[0][0] = row;
-    b[0][1] = column;
-    for (i = 0; i < row; i++)
-    {
-        for (j = 0; j < column; j++)
-        {
-            if (a[i][j] != 0)
-            {
-                b[k][0] = i;
-                b[k][1] = j;
-                b[k][2] = a[i][j];
-                k++;
-            }
-        }
-        b[0][2] = k - 1;
-    }
-}
-
-void print_sparse(int b[MAX][3])
-{
-    int i, column;
-    column = b[0][2];
-    printf("\nSparse matrix\n\n");
-    for (i = 0; i <= column; i++)
-    {
-        printf("%d\t%d\t%d\n", b[i][0], b[i][1], b[i][2]);
-    }
-}
-
