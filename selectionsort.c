ee0e21f44be68ecbab59930142ea21b73ea71b5c
Ashly Rose Antony <75793225+ASHLY0115@users.noreply.github.com>
2021-12-17 10:31:53 +0530

    Add files via upload

--- selectionsort.c
+++ selectionsort.c
@@ -1,22 +1,25 @@
-/*****************************************
-File   :  Selection sort.c
-Author : Ashly Rose Antony
-Date   : 29/11/2021
-Versio :1.0
-********************************************/
+/*********************************
+ file    : Selection sort
+ author  : Ashly Rose Antony
+ version : 1.0
+ date    : 29/11/21
+ **********************************/
 #include <stdio.h>
+
 int main()
 {
-  int array[100], n, c, d, position, t;
+  int array[10], n, c, d, position, t,count=0;
 
   printf("Enter number of elements\n");
+  count++;
   scanf("%d", &n);
-
+  count++;
   printf("Enter %d integers\n", n);
+  count++;
 
-  for (c = 0; c < n; c++)
+  for (c = 0; c < n; c++,count++)
     scanf("%d", &array[c]);
-
+    count++;
   for (c = 0; c < (n - 1); c++) // finding minimum element (n-1) times
   {
     position = c;
@@ -32,12 +35,14 @@ int main()
       array[c] = array[position];
       array[position] = t;
     }
-    
+  }
 
   printf("Sorted list in ascending order:\n");
-
-  for (c = 0; c < n; c++)
+  count++;
+  for (c = 0; c < n; c++,count++)
     printf("%d\n", array[c]);
-
+    count++;
+  printf("\n Time complexity = %d",count);
+  printf("space complexity:60");
   return 0;
 }
