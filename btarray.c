dcf672844dc16f55536aa52d953c3a01e9249d75
Ashly Rose Antony <75793225+ASHLY0115@users.noreply.github.com>
2022-01-28 07:38:49 +0530

    binary tree implementation using arrays

---
+++ btarray.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h> 
+ void insert(int* tree, int size, int element)
+   {
+       if (tree == NULL) return; int pos = 0;
+        while (pos < size)
+        {
+             if (tree[pos]) 
+             { 
+                 if (tree[pos] < element
+                 ) pos = 2 * pos + 2; // right 
+                 else if (tree[pos] && tree[pos] > element)
+                  pos = 2 * pos + 1; // left 
+                  } 
+                  else 
+                  { tree[pos] = element;
+                   return; 
+                   } 
+                 } 
+                } void print(int* tree, int size) 
+                { 
+                    for (int i = 0; i < size; i++)
+                     printf("%d ", tree[i]);
+                      printf("\n");
+                       } 
+                       int main()
+                        {
+                             int tree[100] = {0};
+                              const int tsize = 100;
+                               int n,i,element; 
+                               printf( "Enter the number of elements: "); 
+                               scanf("%d",&n);
+                               printf("Enter the elements : ");
+                                for(i=0;i<n;i++)
+                                {
+                                     scanf("%d",&element); 
+                                     insert(tree, tsize, element);
+                                }
+                                       printf("The binary tree representation using array is : \n"); 
+                                       print(tree, 20);
+                                        return 0;
+    }
